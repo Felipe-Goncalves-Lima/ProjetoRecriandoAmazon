558f416d544e31857c10139e3fa5686f54cf2b86
Felipe-Goncalves-Lima <felipedelima341@gmail.com>
2024-11-01 18:09:57 -0300

    Corrigindo formatação

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,91 +1,97 @@
-import { LogoAmazon, DivLogo, CaixaLogin, FazerLoginTitulo, EmailH4, BotaoContinuar, SpanTermos, 
-    LinkAjuda, InputLogin, SetaDireita, DivLinha, DivContainer, BotaoCriarConta, DivDividerReverso, DivFooter,  
-    SimboloCopyRight, DivCopyRight, SpanDominio} from "./style.jsx"
-import {Divider} from "@mui/material";
+import {
+  LogoAmazon,
+  DivLogo,
+  CaixaLogin,
+  FazerLoginTitulo,
+  EmailH4,
+  BotaoContinuar,
+  SpanTermos,
+  LinkAjuda,
+  InputLogin,
+  SetaDireita,
+  DivLinha,
+  DivContainer,
+  BotaoCriarConta,
+  DivDividerReverso,
+  DivFooter,
+  SimboloCopyRight,
+  DivCopyRight,
+  SpanDominio,
+} from "./style.jsx";
+import { Divider } from "@mui/material";
 
 export const Login = () => {
+  const handleLogin = () => {
+    alert("Entrando...");
+  };
 
-    const handleLogin = () => {
-        alert("Entrando...");
-      };
+  const handleCadastro = () => {
+    alert("Criando nova conta...");
+  };
 
-    const handleCadastro = () => {
-        alert("Criando nova conta...");
-      };
-
-    return(
-
+  return (
     <div>
-
-    <DivContainer>
-
+      <DivContainer>
         <DivLogo>
+          <LogoAmazon src="/logoAmazon.png" alt="Ola" />
 
-            <LogoAmazon src = '/logoAmazon.png' alt = 'Ola' />
-
-            <SpanDominio>.com.br</SpanDominio>
-
+          <SpanDominio>.com.br</SpanDominio>
         </DivLogo>
 
         <CaixaLogin>
+          <FazerLoginTitulo>Fazer Login</FazerLoginTitulo>
 
-            <FazerLoginTitulo>Fazer Login</FazerLoginTitulo>
+          <EmailH4>E-mail ou número de telefone celular</EmailH4>
 
-            <EmailH4>E-mail ou número de telefone celular</EmailH4>
+          <InputLogin type="email" />
 
-            <InputLogin type="email" />
+          <BotaoContinuar onClick={handleLogin}>Continuar</BotaoContinuar>
 
-            <BotaoContinuar onClick={handleLogin}>Continuar</BotaoContinuar>
+          <SpanTermos>
+            Ao continuar, você concorda com as <strong>Condições de Uso</strong>{" "}
+            da Amazon. Por favor verifique a{" "}
+            <strong>Notificação de Privacidade</strong>,
+            <strong>Notificação de Cookies</strong> e a{" "}
+            <strong>Notificação de Anúncios Baseados em Interesse</strong>.
+          </SpanTermos>
 
-            <SpanTermos>Ao continuar, você concorda com as <strong>Condições de Uso</strong> da Amazon. Por favor verifique a <strong>Notificação de Privacidade</strong>, 
+          <LinkAjuda href="">Precisa de ajuda?</LinkAjuda>
 
-            <strong>Notificação de Cookies</strong> e a <strong>Notificação de Anúncios Baseados em Interesse</strong>.</SpanTermos>
-
-            <LinkAjuda href="">Precisa de ajuda?</LinkAjuda>
-
-            <SetaDireita />
-
+          <SetaDireita />
         </CaixaLogin>
 
         <DivLinha>
-
-            <Divider><p>Novo na Amazon?</p></Divider>
-
+          <Divider>
+            <p>Novo na Amazon?</p>
+          </Divider>
         </DivLinha>
 
-        <BotaoCriarConta onClick={handleCadastro}>Criar sua conta da Amazon</BotaoCriarConta>
+        <BotaoCriarConta onClick={handleCadastro}>
+          Criar sua conta da Amazon
+        </BotaoCriarConta>
 
         <DivDividerReverso>
-
-            <Divider></Divider>
-
+          <Divider></Divider>
         </DivDividerReverso>
 
-        <DivFooter  href="">
+        <DivFooter href="">
+          <span>Condições de Uso</span>
 
-            <span>Condições de Uso</span>
+          <span>Política de Privacidade</span>
 
-            <span>Política de Privacidade</span>
+          <span>Ajuda</span>
 
-            <span>Ajuda</span>
+          <span>Cookies</span>
 
-            <span>Cookies</span>
-
-            <span>Anúncios Baseados em Interesses</span>
-
+          <span>Anúncios Baseados em Interesses</span>
         </DivFooter>
 
         <DivCopyRight>
+          <SimboloCopyRight />
 
-            <SimboloCopyRight />
-
-            <span>2021-2024 Amazon.com, Inc. ou suas afiliadas</span>
-
+          <span>2021-2024 Amazon.com, Inc. ou suas afiliadas</span>
         </DivCopyRight>
-        
-     </DivContainer>
-
+      </DivContainer>
     </div>
-
-    )
-}
+  );
+};
